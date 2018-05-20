63843229c033435461169e34ecec036a5185e111
LAPTOP-8UM6O32M\ASUS <yeisonfvc07@gmail.com>
2018-05-19 22:53:27 -0500

    Se crea comunicacion eentre componentes y se trabaja con los hooks

--- zoologico/src/app/app.module.ts
+++ zoologico/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+// COMPONENTES
+import { TiendaComponent } from './components/tienda/tienda.component';
+import { ParquesComponent } from './components/parques/parques.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TiendaComponent,
+    ParquesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ zoologico/src/app/components/parques/parques.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChange, OnInit, DoCheck, OnDestroy } from '@angular/core';
+
+@Component({
+    selector: 'parques',
+    templateUrl: './parques.component.html'
+})
+export class ParquesComponent implements OnChanges, OnInit, DoCheck, OnDestroy {
+    @Input() titulo: string;
+    public metros: number;
+    public vegetacion: string;
+    public abierto: boolean;
+
+    @Output() pasameLosDatos = new EventEmitter();
+
+    constructor() {
+        this.metros = 450;
+        this.vegetacion = 'Alta';
+        this.abierto =  true;
+    }
+    ngOnChanges(changes: SimpleChange) {
+        console.log(changes);
+    }
+    ngOnInit() {
+        console.log('metodo onInit OK');
+    }
+    ngDoCheck() {
+        console.log('El doCkeck se ha ejecutado');
+    }
+    emitirEvento() {
+        this.pasameLosDatos.emit({
+            'titulo': this.titulo,
+            'metros': this.metros,
+            'vegetacion': this.vegetacion,
+            'abierto': this.abierto,
+        });
+    }
+    ngOnDestroy() {
+        console.log('Se va a eliminar el componente');
+    }
+}
