639cc9b917e8a71952d3bcc69fe0229d71220222
LAPTOP-8UM6O32M\ASUS <yeisonfvc07@gmail.com>
2018-05-19 23:06:08 -0500

    crear archivo de rutas

--- zoologico/src/app/app.module.ts
+++ zoologico/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {routing, appRoutingProviders} from './app.routing';
 
 // COMPONENTES
 import { TiendaComponent } from './components/tienda/tienda.component';
@@ -17,9 +18,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
